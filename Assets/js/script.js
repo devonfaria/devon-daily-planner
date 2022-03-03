a7b2d9ad2c1eaa0750f0acefb4767700cab50a95
Devon Faria <devonfaria@gmail.com>
2022-03-03 18:29:50 -0500

    Added starter variables and a function to capture textarea input

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,22 +1,33 @@
-// STORING VARIABLES
+// DEFINING INTERACTABLES
 var saveButton = document.querySelector('.saveBtn');
 var textArea = document.querySelector('.description');
 var dayDisplay = document.getElementById('currentDay');
 
-saveButton.addEventListener('click', function () {
-  console.log('Hi!');
+// STARTING VARIABLES
+var totalScheduleArray = [];
+var inputForStorage = {
+  hour: '',
+  event: '',
+}
+
+// BUTTON FUNCTIONALITY
+saveButton.addEventListener('click', function (event.target) {
+  console.log('captured the text!');
+  captureText();
 });
 
 // STORING FUNCTIONS
+
 // Time Call Function
 var determineTime = function () {
   var currentTime = moment().format('dddd, MMMM Do');
   dayDisplay.innerHTML = currentTime;
 }
 
-var captureText = function () {
-  var inputForStorage = document.querySelector('.description').
-
+// Captures text from input field
+var captureText = function () {  
+  document.querySelector('.description').value;
+  console.log(inputForStorage);
 }
 
 determineTime();
