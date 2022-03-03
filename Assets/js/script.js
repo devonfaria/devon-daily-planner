50a5ef5dd79764e5bcca4e8f7c53f166fe025918
Devon Faria <devonfaria@gmail.com>
2022-03-03 16:52:17 -0500

    added div, textarea, and button to time block, connected script.js, and added css classes to elements

---
+++ Assets/js/script.js
@@ -0,0 +1,7 @@
+// STORING VARIABLES
+var saveButton = document.querySelector('.saveBtn');
+var textArea = document.querySelector('.text');
+
+saveButton.addEventListener('click', function () {
+  console.log('Hi!');
+});
