0f77dd6a3cf2a324d42b1556af6ae2dd3ac37479
Devon Faria <devonfaria@gmail.com>
2022-03-03 18:03:31 -0500

    added date to the header using moment()

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,18 @@
 // STORING VARIABLES
 var saveButton = document.querySelector('.saveBtn');
 var textArea = document.querySelector('.text');
+var dayDisplay = document.getElementById('currentDay');
 
 saveButton.addEventListener('click', function () {
   console.log('Hi!');
 });
+
+// STORING FUNCTIONS
+// Time Call Function
+var determineTime = function () {
+  var currentTime = moment().format('dddd, MMMM Do');
+  dayDisplay.innerHTML = currentTime;
+  dayDisplay.style.backgroundColor = 'black';
+}
+
+determineTime();
